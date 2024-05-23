241bd278abaed83816944159e5c5a4d31e05c04d
Jyotishman Goswami <jyotishman2468@gmail.com>
2024-05-23 20:03:07 +0530

    first-commit

---
+++ first.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var smssendingLimit int
+    // var crossLimiy float64
+	// var hasPermision bool
+	// var username string
+
+	// another way of declaring a string
+	congrats:= "Happy Birthday dear"
+
+	fmt.Println("hi->",congrats,"💥");
+
+	height:= 4;
+
+	if height > 5 {
+		 fmt.Println("🤑",true)
+	} else {
+		  fmt.Println("🦊",false)
+	}
+
+    // fmt.Println("%v %f %v %q\n",smssendingLimit,crossLimiy,hasPermision,username)
+}
+
+
---
+++ fync.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func st(s1 int, s2 int) int {
+	 return s1 + s2
+}
+
+func main(){
+	  fmt.Println("Function")
+	  fmt.Println(st(4,2))
+}
